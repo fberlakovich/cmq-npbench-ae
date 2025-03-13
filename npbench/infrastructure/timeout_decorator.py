37c067402a2b1cdf440931457537ce44da3df012
Felix Berlakovich <felix@berlakovich.at>
2025-03-13 13:13:01 +0100

    fix timeout bug that failed AE #1

--- npbench/infrastructure/timeout_decorator.py
+++ npbench/infrastructure/timeout_decorator.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 from __future__ import print_function
 
+import _thread
 import sys
 import threading
 from time import sleep
@@ -47,7 +48,7 @@ def cdquit(fn_name):
     # print to stderr, unbuffered in Python 2.
     print('{0} took too long'.format(fn_name), file=sys.stderr)
     sys.stderr.flush()  # Python 3 stderr is likely buffered.
-    thread.interrupt_main()  # raises KeyboardInterrupt
+    _thread.interrupt_main()  # raises KeyboardInterrupt
 
 
 def exit_after(s):
